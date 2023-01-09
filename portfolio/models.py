7a31173dfb39370b4f829f76a6854db7a49e1078
Vadim <vadya.dmitrichev26@gmail.com>
2023-01-09 19:40:50 +0300

    add studying section and buttons

--- portfolio/models.py
+++ portfolio/models.py
@@ -9,3 +9,13 @@ class Project(models.Model):
 
 	def __str__(self):
 		return self.title
+
+
+class Theory(models.Model):
+	title = models.CharField(max_length=100)
+	description = models.CharField(max_length=250)
+	image = models.ImageField(upload_to='portfolio/images/')
+	url = models.URLField(blank=True)
+
+	def __str__(self):
+		return self.title
